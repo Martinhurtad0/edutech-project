b58d3817147fd2ce66ee0ffc1b57953714c93be4
Martinhurtad0 <bmhurtadon@gmail.com>
2023-10-27 07:50:51 -0500

    carpetas organizadas solo el html y css con las vistas

---
+++ view/layout/footer.php
@@ -0,0 +1,30 @@
+<footer>
+      <div class="redes-sociales">
+        <a href="#"
+          ><img src="../resource/img/icons/icons8-whatsapp-50.png" alt="whatsapp"
+        /></a>
+        <a href="#"
+          ><img src="../resource/img/icons/icons8-instagram-50.png" alt="instagram"
+        /></a>
+        <a href="#"
+          ><img src="../resource/img/icons/icons8-facebook-nuevo-50.png" alt="facebook"
+        /></a>
+      </div>
+      <div class="info">
+        <div>
+          <img src="../resource/img/icons/logo-kepler-removebg-preview.png" alt="logo" />
+        </div>
+        <div class="contacto">
+          <div class="ubicacion">
+            <h4>UBICACION</h4>
+            <p>Sogamoso-Boyacá</p>
+            <p>carrera 11 #20-130 Barrio el Paraiso</p>
+          </div>
+          <div>
+            <h4>CONTACTO</h4>
+            <p>3123467007</p>
+            <p>kepler123@gmail.edu.co</p>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ view/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar secion</title>
+    <link rel="stylesheet" href="../resource/css/login/login.css">
+    
+</head>
+<body>
+   
+    <div class="log-container">
+    <div class="cuenta-container">
+     <h1> <a href="../index.php"> <img src="../resource/img/updatesale_login_signup/LOGOLOGIN.png" alt=""> </a> KEPLER <br> EDUCATION</h1>
+     <h3>Gracias por volver!</h3>
+     <div class="registro-container">
+        
+      <a href="" >iniciar sesion</a>
+      <a rel="stylesheet" href="sign_up_user.html" >Registrate</a>
+
+     </div>
+     <div class="formulario">
+     <form action="">
+        <label for="correo">correo electronico</label>
+        <input type="email" id="correo" required>
+        <label for="contraseña">Contraseña</label>
+        <input type="password" id="contraseña" required>
+        <a href=""> Olvidaste tu contraseña?</a>
+        <button type="submit"> <a href="../html_brayan/vista_admin.html"  id="enviar">LOGIN</a></button>
+        
+     </form>
+    </div>
+    </div>
+ <div class="image">
+        <img src="../resource/img/updatesale_login_signup/computo.png" alt="">
+ </div>
+
+
+    </div>
+    
+</body>
+</html>
